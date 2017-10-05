7774bf48ab95b42f3f9dca92e2f1ceb8fe2725a2
Tit Petric <black@scene-si.org>
2017-10-05 14:45:45 +0200

    update build

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-go fmt
-docker run --rm -it -e CGO_ENABLED=0 -v `pwd`:/go/src/app -w /go/src/app golang:1.9-alpine go build -o undock *.go
+set -e
+rm -rf build
+GOOSES=${1:-"darwin linux"}
+GOARCHS=${2:-"amd64 386"}
+for GOOS in $GOOSES; do
+	for GOARCH in $GOARCHS; do
+		echo "Building $GOOS/$GOARCH"
+		if [ "$GOOS" == "windows" ]; then
+			docker run --rm -e CGO_ENABLED=0 -e GOOS=$GOOS -e GOARCH=$GOARCH -v `pwd`:/go/src/app -w /go/src/app golang:1.9-alpine go build -o build/undock-$GOOS-$GOARCH.exe .
+		else
+			docker run --rm -e CGO_ENABLED=0 -e GOOS=$GOOS -e GOARCH=$GOARCH -v `pwd`:/go/src/app -w /go/src/app golang:1.9-alpine go build -o build/undock-$GOOS-$GOARCH .
+			if [ "$GOOS" == "linux" ]; then
+				strip build/undock-$GOOS-$GOARCH
+			fi
+		fi
+	done
+done
+gzip -k build/*
