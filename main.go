5709399caa5de18b8232606083c6a134eb510a99
Tit Petric <black@scene-si.org>
2017-10-05 13:07:38 +0200

    clean tabs and spaces a bit

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"log"
 	"os"
 	"path"
+	"strings"
 
-	"github.com/davecgh/go-spew/spew"
+	_ "github.com/davecgh/go-spew/spew"
 	"github.com/moby/moby/builder/dockerfile/parser"
 )
 
@@ -19,26 +20,38 @@ func main() {
 	result, err := parser.Parse(f)
 	f.Close()
 
+	clean := func(s string) string {
+		s = strings.Replace(s, "\t", " ", -1)
+		s = strings.Replace(s, "  ", " ", -1)
+		return s
+	}
+
+	fmt.Println("#!/bin/bash")
 	for _, value := range result.AST.Children {
-		fmt.Printf("# %s", value.Original)
+		fmt.Printf("# %s", clean(value.Original))
 		switch value.Value {
 		case "add":
 			fmt.Println()
 			file := path.Join(wd, value.Next.Value)
 			destination := value.Next.Next.Value
-
 			if _, err := os.Stat(file); os.IsNotExist(err) {
 				log.Fatal(err)
 			}
 			fmt.Println("cp", file, destination)
 		case "copy":
 			fmt.Println()
-			spew.Dump(value, wd)
+			file := path.Join(wd, value.Next.Value)
+			destination := value.Next.Next.Value
+			if value.Next.Value[len(value.Next.Value)-1] == '/' {
+				file += "/"
+			}
+			fmt.Println("rsync -ia", file, destination)
 		case "run":
 			fmt.Println()
-			spew.Dump(value, wd)
+			fmt.Println(clean(value.Next.Value))
 		default:
 			fmt.Println(" -- not implemented (" + value.Value + ")")
+			// spew.Dump(value, wd)
 		}
 	}
 }
